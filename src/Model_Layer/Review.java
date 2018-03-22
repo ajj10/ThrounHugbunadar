fd9467bc89f0fc4cb67cb76f15cdf820883a8dd6
Adam <Adam@Adam-PC.lan>
2018-03-22 14:28:13 +0000

    a

--- src/Model_Layer/Review.java
+++ src/Model_Layer/Review.java
@@ -1,5 +1,43 @@
 package Model_Layer;
 
+//import java.sql.Date;
+import java.util.Date;
+
 public class Review {
+	
+	private String author;
+	private String title;
+	private Date date;
+	private String reviewText;
+	private int rating;
+	
+	public Review(String reviewAuthor, String reviewTitle, Date reviewDate, String ReviewText, int reviewRating) 
+	{
+		author = reviewAuthor;
+		title = reviewTitle;
+		date = reviewDate;
+		reviewText = ReviewText;
+		rating = reviewRating;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public String getReviewText() {
+		return reviewText;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
 
 }
