b34a82f1e3e76efa84ed0a714759c8cd07e2eeeb
Thor <ths220@hi.is>
2018-04-03 11:55:57 +0000

    test

--- test/Controller_Layer1/TripSearchTest.java
+++ test/Controller_Layer1/TripSearchTest.java
@@ -3,21 +3,28 @@ package Controller_Layer1;
 import org.junit.*;
 import org.junit.jupiter.api.Test;
 
+import Controller_Layer.TripSearch;
+import Model_Layer.Daytrip;
+
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 public class TripSearchTest {
+	private TripSearch mySearch;
 	@Before
 	public void setUp() {
-		
+		mySearch = new TripSearch(null, "Reykjavík", null, 0, 100000, -1, -1);
+		mySearch.updateTrips();
 	}
 	
 	@After
 	public void tearDown() {
-		
+		mySearch = null;
 	}
 	
 	@Test
 	public void testSomeBehavior() {
-	
+		assertEquals("Reykjavík","Reykjavík");
 	}
 }
