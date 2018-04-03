2624efb9794f0a90f22f1376b268a5ade841d927
Thor <ths220@hi.is>
2018-04-03 11:04:48 +0000

    a

---
+++ test/Controller_Layer/TripSearchTest.java
@@ -0,0 +1,21 @@
+package Controller_Layer;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TripSearchTest {
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void testSomeBehavior() {
+	
+	}
+}
