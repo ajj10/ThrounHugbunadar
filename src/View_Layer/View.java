bce49528b8cded65377ce4b2a17b27e7ccb5617b
Adam <Adam@130.208.151.108>
2018-04-05 12:36:36 +0000

    view

--- src/View_Layer/View.java
+++ src/View_Layer/View.java
@@ -2,26 +2,54 @@ package View_Layer;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+
+import Model_Layer.Basket;
 import Model_Layer.Daytrip;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
+
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+
+import Controller_Layer.Root;
+import Controller_Layer.TripSearch;
+
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import java.awt.ScrollPane;
+import java.awt.Panel;
+import java.awt.List;
+import java.awt.Scrollbar;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class View {
 	
-	private Daytrip[] trips;
+	private ArrayList<Daytrip> trips;
+	private TripSearch tripsearch = new TripSearch(trips, "", "", 0, 10000000, -1, -1);
+	private Basket basket;
+	private Root root = new Root(basket, tripsearch);
 	
 	private JFrame frame;
 	private JTextField searchBoxTextField;
 	
 	public String searchString;
-
+	public String Location = "Reykjavík";
+	public int index;
+	
+	
+	
 	/**
 	 * Launch the application.
 	 */
@@ -43,6 +71,8 @@ public class View {
 	 */
 	public View() {
 		initialize();
+		
+		
 	}
 
 	/**
@@ -55,21 +85,94 @@ public class View {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Search");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton searchButton = new JButton("Search");
+		searchButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				searchString = searchBoxTextField.getText();
 				System.out.println(searchString);
+				
 			}
 		});
 		
 		
-		btnNewButton.setBounds(341, 11, 83, 23);
-		frame.getContentPane().add(btnNewButton);
+		searchButton.setBounds(341, 11, 83, 23);
+		frame.getContentPane().add(searchButton);
 		
 		searchBoxTextField = new JTextField();
 		searchBoxTextField.setBounds(22, 12, 299, 20);
 		frame.getContentPane().add(searchBoxTextField);
 		searchBoxTextField.setColumns(10);
+		
+		JComboBox comboBoxLocation = new JComboBox();
+		comboBoxLocation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Location = (String)comboBoxLocation.getSelectedItem();
+				System.out.println(Location);
+				int[] priceRange = new int[2];
+				priceRange[0] = 0;
+				priceRange[1] = 10000000;
+				
+					try {
+						trips = root.findTrips(Location, null, priceRange, -1, -1);
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				System.out.println(trips.get(0).getName());
+				//tripDisplayList.setModel(dm);
+			}
+		});
+		comboBoxLocation.addItem("Reykjavík");
+		comboBoxLocation.addItem("Akureyri");
+		comboBoxLocation.addItem("Selfoss");
+		comboBoxLocation.addItem("Ísafjörður");
+		comboBoxLocation.addItem("Egilsstaðir");
+		//comboBoxLocation.setSelectedItem(null);
+		comboBoxLocation.setToolTipText("");
+		comboBoxLocation.setBounds(341, 58, 83, 22);
+		frame.getContentPane().add(comboBoxLocation);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(22, 42, 299, 167);
+		frame.getContentPane().add(scrollPane);
+		
+		JList tripDisplayList = new JList();
+		tripDisplayList.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				index = tripDisplayList.getSelectedIndex();
+				String nafn = (String)tripDisplayList.getSelectedValue();
+				System.out.println(nafn);
+			}
+		});
+		scrollPane.setViewportView(tripDisplayList);
+		DefaultListModel dm = new DefaultListModel();
+		
+		
+		for(int i = 0; i<trips.size(); i++) {
+			dm.addElement(trips.get(i).getName());
+		}
+	
+		dm.addElement("palli");
+		dm.addElement("halli");
+		dm.addElement("salli");
+		dm.addElement("dalli");
+		dm.addElement("ralli");
+		dm.addElement("talli");
+		dm.addElement("lalli");
+		dm.addElement("jalli");
+		dm.addElement("galli");
+		dm.addElement("falli");
+		dm.addElement("salli");
+		dm.addElement("valli");
+		dm.addElement("balli");
+		dm.addElement("nalli");
+		dm.addElement("malli");
+		dm.addElement("zalli");
+		dm.addElement("walli");
+		
+		tripDisplayList.setModel(dm);
+		
+		
 	}
 }
