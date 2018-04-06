6ac8872a8038ce143ea434e9862850552b088147
isak <isak@tg-dw115.rhi.hi.is>
2018-04-06 12:39:26 +0000

    eh útlitsbreytingar

--- src/View_Layer/View_Trip.java
+++ src/View_Layer/View_Trip.java
@@ -6,12 +6,25 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import Controller_Layer.TripSearch;
+
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class View_Trip extends JFrame {
 
+	// breyting
+	private TripSearch tripsearch;
+	
 	private JPanel contentPane;
+	private JTextField Activity_Text;
+	private JTextField Location_Text;
+	private JTextField Price_Text;
+	private JTextField Rating_Text;
+	private JTextField Duration_Text;
+	
 
 	/**
 	 * Launch the application.
@@ -33,6 +46,7 @@ public class View_Trip extends JFrame {
 	 * Create the frame.
 	 */
 	public View_Trip() {
+		setTitle("bla");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -45,27 +59,58 @@ public class View_Trip extends JFrame {
 		contentPane.add(photoPanel);
 		
 		JTextArea descriptionText = new JTextArea();
+		descriptionText.setEditable(false);
 		descriptionText.setBounds(148, 11, 262, 92);
 		contentPane.add(descriptionText);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(10, 131, 46, 14);
+		JLabel lblNewLabel = new JLabel("Activity:");
+		lblNewLabel.setBounds(10, 131, 62, 14);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(10, 156, 46, 14);
+		JLabel lblNewLabel_1 = new JLabel("Location:");
+		lblNewLabel_1.setBounds(10, 156, 62, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(10, 181, 46, 14);
+		JLabel lblNewLabel_2 = new JLabel("Price:");
+		lblNewLabel_2.setBounds(10, 181, 54, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setBounds(10, 206, 46, 14);
+		JLabel lblNewLabel_3 = new JLabel("Rating:");
+		lblNewLabel_3.setBounds(10, 206, 54, 14);
 		contentPane.add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		lblNewLabel_4.setBounds(10, 231, 46, 14);
+		JLabel lblNewLabel_4 = new JLabel("Duration:");
+		lblNewLabel_4.setBounds(10, 231, 62, 14);
 		contentPane.add(lblNewLabel_4);
+		
+		Activity_Text = new JTextField();
+		Activity_Text.setEditable(false);
+		Activity_Text.setBounds(84, 125, 130, 26);
+		contentPane.add(Activity_Text);
+		Activity_Text.setColumns(10);
+		
+		Location_Text = new JTextField();
+		Location_Text.setEditable(false);
+		Location_Text.setColumns(10);
+		Location_Text.setBounds(84, 150, 130, 26);
+		contentPane.add(Location_Text);
+		
+		Price_Text = new JTextField();
+		Price_Text.setEditable(false);
+		Price_Text.setColumns(10);
+		Price_Text.setBounds(84, 175, 130, 26);
+		contentPane.add(Price_Text);
+		
+		Rating_Text = new JTextField();
+		Rating_Text.setEditable(false);
+		Rating_Text.setColumns(10);
+		Rating_Text.setBounds(84, 200, 130, 26);
+		contentPane.add(Rating_Text);
+		
+		Duration_Text = new JTextField();
+		Duration_Text.setEditable(false);
+		Duration_Text.setColumns(10);
+		Duration_Text.setBounds(84, 225, 130, 26);
+		contentPane.add(Duration_Text);
 	}
 }
